7228692c480d96e9e028d83f8027815700fc466a
Imanol Rodriguez <imanol_2005@icloud.com>
2025-02-22 19:11:39 -0500

    Estructura principal del proyecto

---
+++ Tarea 5/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="witdh-device-witdh, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css rel="stylesheet">
+        <title>Portal Web en PHP con 10 APIs Externas😎</title>
+
+        
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="mt-5">Portal Web en PHP con 10 APIs Externas😎</h1>
+            <p>Imanol Rodriguez</p>
+            <img src="https://passport-photo.online/images/cms/a7f0bbb8e693e46ad7f710344312f3df_3d7812395b.webp?quality=80&format=webp&width=1920" alt="Imanol Rodriguez" class="img-fluid">
+        </div>
+            
+</html>
